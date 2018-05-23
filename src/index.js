d95fad639e8bf4a01259ea8478a6674074d1bfd1
DongbeenSeo <puus1021@gmail.com>
2018-05-23 13:06:24 +0900

    게시글페이지 추가

--- src/index.js
+++ src/index.js
@@ -1,22 +1,43 @@
 import axios from 'axios'
 
 const rootEl = document.querySelector('.root')
+
 const templates = {
     postList: document.querySelector('#post-list').content,
-    postItem: document.querySelector('#post-item').content
-}
+    postItem: document.querySelector('#post-item').content,
+    postContent: document.querySelector('#post-content').content
+};
 
+function render(fragment) {
+    rootEl.textContent = '';
+    rootEl.appendChild(fragment);
+}
 async function indexPage() {
     const res = await axios.get('http://localhost:3000/posts');
     const listFragment = document.importNode(templates.postList, true);
     res.data.forEach(post => {
         const fragment = document.importNode(templates.postItem, true);
-        const pEl = fragment.querySelector('.post-item__title')
+        const pEl = fragment.querySelector('.post-item__title');
         pEl.textContent = post.title;
+        pEl.addEventListener('click', e => {
+            postContentPage(post.id);
+        })
         listFragment.querySelector('.post-list').appendChild(fragment);
-    })
+    });
+    render(listFragment);
+}
 
-    rootEl.appendChild(listFragment);
+
+async function postContentPage(postID) {
+    const res = await axios.get(`http://localhost:3000/posts/${postID}`)
+    const fragment = document.importNode(templates.postContent, true);
+
+    fragment.querySelector('.post-content__title').textContent = `제목: ${res.data.title}`;
+    fragment.querySelector('.post-content__body').textContent = `내용: ${res.data.body}`;
+    fragment.querySelector('.post-content__btn-back').addEventListener('click', e => {
+        indexPage();
+    })
+    render(fragment);
 }
 
 indexPage();
