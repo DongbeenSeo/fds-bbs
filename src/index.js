23863accd99d0de04a8b973e51c73c4d07c8e0c1
DongbeenSeo <puus1021@gmail.com>
2018-05-23 15:22:38 +0900

    login페이지

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import axios from 'axios'
 
+const postAPI = axios.create({})
+
 const rootEl = document.querySelector('.root')
 
 const templates = {
     postList: document.querySelector('#post-list').content,
     postItem: document.querySelector('#post-item').content,
-    postContent: document.querySelector('#post-content').content
+    postContent: document.querySelector('#post-content').content,
+    login: document.querySelector('#login').content
 };
 
 function render(fragment) {
@@ -13,8 +16,12 @@ function render(fragment) {
     rootEl.appendChild(fragment);
 }
 async function indexPage() {
-    const res = await axios.get('http://localhost:3000/posts');
+    const res = await postAPI.get('http://localhost:3000/posts');
     const listFragment = document.importNode(templates.postList, true);
+
+    listFragment.querySelector('.post-list__login-btn').addEventListener('click', e => {
+        loginPage();
+    })
     res.data.forEach(post => {
         const fragment = document.importNode(templates.postItem, true);
         const pEl = fragment.querySelector('.post-item__title');
@@ -24,12 +31,13 @@ async function indexPage() {
         })
         listFragment.querySelector('.post-list').appendChild(fragment);
     });
+
     render(listFragment);
 }
 
 
 async function postContentPage(postID) {
-    const res = await axios.get(`http://localhost:3000/posts/${postID}`)
+    const res = await postAPI.get(`http://localhost:3000/posts/${postID}`)
     const fragment = document.importNode(templates.postContent, true);
 
     fragment.querySelector('.post-content__title').textContent = `제목: ${res.data.title}`;
@@ -40,4 +48,25 @@ async function postContentPage(postID) {
     render(fragment);
 }
 
+
+async function loginPage() {
+    const fragment = document.importNode(templates.login, true);
+    const formEl = fragment.querySelector('.login__form');
+
+    formEl.addEventListener('submit', async e => {
+        const payload = {
+            //e.target에는 formEl이 포함
+            //e.target.elements.username.value === fragment.querySelector('.login__username');
+            username: e.target.elements.username.value,
+            password: e.target.elements.password.value
+        };
+        e.preventDefault();
+        const res = await postAPI.post('http://localhost:3000/users/login', payload)
+        localStorage.setItem('token', res.data.token);
+        postAPI.defaults.headers['Authorizaion'] = res.data.token;
+        indexPage();
+    })
+
+    render(fragment);
+}
 indexPage();
